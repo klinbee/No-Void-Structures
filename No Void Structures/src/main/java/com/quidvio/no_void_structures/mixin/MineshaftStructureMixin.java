fd6fd2e33aeacf12e550972a9e2467e1b1950425
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-03-24 18:46:51 -0500

    finalized mineshaft gen

--- No Void Structures/src/main/java/com/quidvio/no_void_structures/mixin/MineshaftStructureMixin.java
+++ No Void Structures/src/main/java/com/quidvio/no_void_structures/mixin/MineshaftStructureMixin.java
@@ -20,11 +20,42 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MineshaftStructure.class)
 public class MineshaftStructureMixin {
 
-    private int height;
+    private int surfaceHeight;
+
     @Shadow @Final
     private
-    MineshaftStructure.Type type;
+    MineshaftStructure.Type type; // Used to get the mineshaft piece.
 
+
+    /**
+     * Stops Mineshafts from generating as low in the world.
+     *
+     * The Mineshaft height generation is based on the sea-level... void worlds tend to have atypical sea-levels.
+     *
+     * Changes it to be from 32 blocks below the surface height and 64 blocks below the surface height.
+     *
+     * @param instance the Chunk generator, used to get the minimum y.
+     * @return the new modified y-level basis for Mineshaft generation.
+     */
+    @Redirect(method = "Lnet/minecraft/world/gen/structure/MineshaftStructure;addPieces(Lnet/minecraft/structure/StructurePiecesCollector;Lnet/minecraft/world/gen/structure/Structure$Context;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/ChunkGenerator;getSeaLevel()I"))
+    private int no_void_structure_mineshaftHeightFix_MS(ChunkGenerator instance) {
+        return surfaceHeight-32;
+    }
+
+    @Redirect(method = "Lnet/minecraft/world/gen/structure/MineshaftStructure;addPieces(Lnet/minecraft/structure/StructurePiecesCollector;Lnet/minecraft/world/gen/structure/Structure$Context;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/ChunkGenerator;getMinimumY()I"))
+    private int no_void_structure_mineshaftHeight_MS(ChunkGenerator instance) {
+        return surfaceHeight-64;
+    }
+
+    /**
+     * Gets the surface height at the area where the Mineshaft attempts to generate.
+     *
+     * Used for the method above to change the generation range.
+     *
+     * @param collector default usage
+     * @param context default usage
+     * @param cir unused
+     */
     @Inject(method = "Lnet/minecraft/world/gen/structure/MineshaftStructure;addPieces(Lnet/minecraft/structure/StructurePiecesCollector;Lnet/minecraft/world/gen/structure/Structure$Context;)I", at = @At("HEAD"))
     private void no_void_structure_getMineshaftHeight_MS(StructurePiecesCollector collector, Structure.Context context, CallbackInfoReturnable<Integer> cir) {
         ChunkPos chunkPos = context.chunkPos();
@@ -35,30 +66,8 @@ public class MineshaftStructureMixin {
         collector.addPiece(mineshaftRoom);
         mineshaftRoom.fillOpenings(mineshaftRoom, collector, chunkRandom);
         if (!collector.isEmpty()) {
-            height = context.chunkGenerator().getHeight(collector.getBoundingBox().getCenter().getX(), collector.getBoundingBox().getCenter().getZ(), Heightmap.Type.WORLD_SURFACE_WG, context.world(), context.noiseConfig());
+            surfaceHeight = context.chunkGenerator().getHeight(collector.getBoundingBox().getCenter().getX(), collector.getBoundingBox().getCenter().getZ(), Heightmap.Type.WORLD_SURFACE_WG, context.world(), context.noiseConfig());
         }
-    }
-
-
-
-    /**
-     * Stops Mineshafts from generating as low in the world.
-     *
-     * The Mineshaft height generation is based on the sea-level... void worlds tend to have atypical sea-levels.
-     *
-     * Changes it to be 128 blocks above the minimum y (64 by default).
-     *
-     * @param instance the Chunk generator, used to get the minimum y.
-     * @return the new modified y-level basis for Mineshaft generation.
-     */
-    @Redirect(method = "Lnet/minecraft/world/gen/structure/MineshaftStructure;addPieces(Lnet/minecraft/structure/StructurePiecesCollector;Lnet/minecraft/world/gen/structure/Structure$Context;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/ChunkGenerator;getSeaLevel()I"))
-    private int no_void_structure_mineshaftHeightFix_MS(ChunkGenerator instance) {
-        return height-32;
-    }
-
-    @Redirect(method = "Lnet/minecraft/world/gen/structure/MineshaftStructure;addPieces(Lnet/minecraft/structure/StructurePiecesCollector;Lnet/minecraft/world/gen/structure/Structure$Context;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/ChunkGenerator;getMinimumY()I"))
-    private int no_void_structure_mineshaftHeight_MS(ChunkGenerator instance) {
-        return height-32;
     }
 
 }
