d0c225db472af70d71ec01ac68287554a1609b81
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-04-04 21:43:17 -0500

    added forge base

---
+++ No Void Structures FORGE/src/main/java/com/quidvio/no_void_structures/No_void_structures.java
@@ -0,0 +1,14 @@
+package com.quidvio.no_void_structures;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(No_void_structures.MODID)
+public class No_void_structures {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "no_void_structures";
+    public No_void_structures() {
+    }
+
+}
