a7e5458de9c051956619fd56bd002920d74dec1b
Caleb Raschka <craschka@wisc.edu>
2024-04-29 10:11:34 -0500

    Backport to 1.19.2 Forge, Added Aetherial Islands to dev environment.

--- No Void Structures - FORGE/src/main/java/com/quidvio/no_void_structures_mod/mixin/StructureMixin.java
+++ No Void Structures - FORGE/src/main/java/com/quidvio/no_void_structures_mod/mixin/StructureMixin.java
@@ -1,10 +1,16 @@
 package com.quidvio.no_void_structures_mod.mixin;
 
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
 
 @Mixin(Structure.class)
 public class StructureMixin {
@@ -19,13 +25,15 @@ public class StructureMixin {
      * Otherwise, uses the current return value.
      *
      * @param generationStub default usage, also used to get the gen height
-     * @param generationContext default usage, also used to get the void height
+     * @param chunkGenerator default usage, also used to get the void height
+     * @param randomState unused
+     * @param pred unused
      * @param cir the current return value for the isValidBiome check
      */
-    @Inject(method = "Lnet/minecraft/world/level/levelgen/structure/Structure;isValidBiome(Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationStub;Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationContext;)Z", at = @At("RETURN"), cancellable = true)
-    private static void no_void_structures_stopGenericStructureVoidGen_S(Structure.GenerationStub generationStub, Structure.GenerationContext generationContext, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "Lnet/minecraft/world/level/levelgen/structure/Structure;isValidBiome(Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationStub;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/RandomState;Ljava/util/function/Predicate;)Z", at = @At("RETURN"), cancellable = true)
+    private static void no_void_structures_stopGenericStructureVoidGen_S(Structure.GenerationStub generationStub, ChunkGenerator chunkGenerator, RandomState randomState, Predicate<Holder<Biome>> pred, CallbackInfoReturnable<Boolean> cir) {
         int yPos = generationStub.position().getY();
-        if (yPos <= generationContext.chunkGenerator().getMinY() + 8) {
+        if (yPos <= chunkGenerator.getMinY() + 8) {
             cir.setReturnValue(false);
         }
         cir.setReturnValue(cir.getReturnValue());
